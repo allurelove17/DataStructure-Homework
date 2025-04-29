4f4f84dd95963dd748743d69752fe00cb2abd0d9
allurelove17 <66318780+allurelove17@users.noreply.github.com>
2025-04-29 22:00:25 +0800

    Add files via upload

--- homework1/src/composite.hpp
+++ homework1/src/composite.hpp
@@ -1,23 +1,15 @@
-#ifndef COMPOSITE_HPP
-#define COMPOSITE_HPP
+#ifndef COMPOSITE_SORT_HPP
+#define COMPOSITE_SORT_HPP
 
 template<class T>
 class composite_sort {
 public:
     composite_sort(int SIZE) {
         array = new T[SIZE];
-        quick_sorter = new quick<T>(SIZE);
-        heap_sorter = new heap<T>(SIZE);
-        merge_sorter = new Merge<T>(SIZE);
-        insertion_sorter = new insertion<T>(SIZE);
     }
 
     ~composite_sort() {
         delete[] array;
-        delete quick_sorter;
-        delete heap_sorter;
-        delete merge_sorter;
-        delete insertion_sorter;
     }
 
     void print(int size) {
@@ -27,41 +19,115 @@ public:
         std::cout << '\n';
     }
 
+    void getMemoryUsage(SIZE_T& workingSetSize) {
+        PROCESS_MEMORY_COUNTERS_EX memInfo;
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&memInfo, sizeof(memInfo));
+        workingSetSize = memInfo.PrivateUsage;
+    }
+
     double sort(int size, SIZE_T& memory_used) {
+        SIZE_T beforeMemory = 0, afterMemory = 0;
+        getMemoryUsage(beforeMemory);
+
         auto start = std::chrono::high_resolution_clock::now();
 
         if (size <= 75) {
-            for (int i = 0; i < size; ++i) {
-                insertion_sorter->array[i] = array[i];
-            }
-            insertion_sorter->insertion_sort(size);
-            for (int i = 0; i < size; ++i) {
-                array[i] = insertion_sorter->array[i];
-            }
-            memory_used = size * sizeof(T) + sizeof(T);
+            insertion_sort(0, size - 1);
         }
         else {
-            for (int i = 0; i < size; ++i) {
-                quick_sorter->array[i] = array[i];
-            }
-            SIZE_T quick_memory = 0;
-            quick_sorter->quicktime(0, size - 1, quick_memory);
-            for (int i = 0; i < size; ++i) {
-                array[i] = quick_sorter->array[i];
-            }
-            memory_used = quick_memory;
+            int depth = 0, max_depth = 0;
+            quick_sort(0, size - 1, depth, max_depth);
+            memory_used = max_depth * (3 * sizeof(int) + sizeof(T));
         }
 
         auto end = std::chrono::high_resolution_clock::now();
+
+        getMemoryUsage(afterMemory);
+        if (afterMemory > beforeMemory) {
+            memory_used = afterMemory - beforeMemory;
+        }
+
         return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     }
 
+    void insertion_sort(int left, int right) {
+        for (int i = left + 1; i <= right; ++i) {
+            T value = array[i];
+            int j = i - 1;
+
+            while (j >= left && value < array[j]) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = value;
+        }
+    }
+
+    int findMedianOfThree(int left, int mid, int right) {
+        T a = array[left];
+        T b = array[mid];
+        T c = array[right];
+
+        if ((a <= b && b <= c) || (c <= b && b <= a))
+            return mid;
+        else if ((b <= a && a <= c) || (c <= a && a <= b))
+            return left;
+        else
+            return right;
+    }
+
+    int pivot(int left, int right) {
+        if (right - left > 2) {
+            int mid = left + (right - left) / 2;
+            int medianIndex = findMedianOfThree(left, mid, right);
+
+            if (medianIndex != right) {
+                T temp = array[medianIndex];
+                array[medianIndex] = array[right];
+                array[right] = temp;
+            }
+        }
+
+        T p = array[right];
+        int i = left - 1, j = left;
+
+        while (j < right) {
+            if (array[j] < p) {
+                ++i;
+                T c = array[i];
+                array[i] = array[j];
+                array[j] = c;
+            }
+            ++j;
+        }
+
+        ++i;
+        T c = array[i];
+        array[i] = array[right];
+        array[right] = c;
+
+        return i;
+    }
+
+    void quick_sort(int left, int right, int depth, int& max_depth) {
+        if (depth > max_depth) {
+            max_depth = depth;
+        }
+        
+        if (right - left <= 16) {
+            insertion_sort(left, right);
+            return;
+        }
+
+        if (left < right) {
+            int p = pivot(left, right);
+            quick_sort(left, p - 1, depth + 1, max_depth);
+            quick_sort(p + 1, right, depth + 1, max_depth);
+        }
+    }
+
     T* array;
-private:
-    quick<T>* quick_sorter;
-    heap<T>* heap_sorter;
-    Merge<T>* merge_sorter;
-    insertion<T>* insertion_sorter;
 };
 
 #endif
