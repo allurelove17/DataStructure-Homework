d1c54a229fd7a78b366d0cb012cd4ad93d31f3c8
allurelove17 <66318780+allurelove17@users.noreply.github.com>
2025-04-29 21:57:32 +0800

    Update composite.hpp

--- homework1/src/composite.hpp
+++ homework1/src/composite.hpp
@@ -53,7 +53,7 @@ public:
         }
 
         auto end = std::chrono::high_resolution_clock::now();
-        return std::chrono::duration<double, std::milli>(end - start).count();
+        return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     }
 
     T* array;
